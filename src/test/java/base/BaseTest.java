2f6c0ac4dca85ef57315f26691fc421f48f86c41
Hoang Cong Truong Loc <hctloc246@gmail.com>
2026-03-31 14:51:07 +0700

    fix: add firefox support for matrix strategy

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -3,6 +3,8 @@ package base;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.testng.ITestResult;
 import org.testng.annotations.*;
 
@@ -15,61 +17,74 @@ public class BaseTest {
 
     private static ThreadLocal<WebDriver> driver = new ThreadLocal<>();
 
-    public WebDriver getDriver(){
+    public WebDriver getDriver() {
         return driver.get();
     }
 
-    @Parameters({"browser","env"})
+    @Parameters({"browser", "env"})
     @BeforeMethod
     public void setUp(@Optional("chrome") String browser,
-                      @Optional("dev") String env){
+                      @Optional("dev") String env) {
 
-        if(browser.equalsIgnoreCase("chrome")){
+        boolean isCI = System.getenv("CI") != null;
 
-    ChromeOptions options = new ChromeOptions();
+        if (browser.equalsIgnoreCase("chrome")) {
+            ChromeOptions options = new ChromeOptions();
 
-    boolean isCI = System.getenv("CI") != null;
+            if (isCI) {
+                options.addArguments("--headless=new");
+                options.addArguments("--no-sandbox");
+                options.addArguments("--disable-dev-shm-usage");
+                options.addArguments("--window-size=1920,1080");
+            } else {
+                options.addArguments("--start-maximized");
+            }
 
-    if (isCI) {
-        options.addArguments("--headless=new");
-        options.addArguments("--no-sandbox");
-        options.addArguments("--disable-dev-shm-usage");
-        options.addArguments("--window-size=1920,1080");
-    }
+            driver.set(new ChromeDriver(options));
 
-    driver.set(new ChromeDriver(options));
-}
+        } else if (browser.equalsIgnoreCase("firefox")) {
+            FirefoxOptions options = new FirefoxOptions();
 
-        getDriver().manage().window().maximize();
+            if (isCI) {
+                options.addArguments("-headless");
+            }
+
+            driver.set(new FirefoxDriver(options));
+
+        } else {
+            throw new IllegalArgumentException("Browser không được hỗ trợ: " + browser);
+        }
+
+        if (!browser.equalsIgnoreCase("chrome") || !isCI) {
+            getDriver().manage().window().maximize();
+        }
     }
 
     @AfterMethod
     public void tearDown(ITestResult result) {
 
-        if(result.getStatus() == ITestResult.FAILURE){
+        if (result.getStatus() == ITestResult.FAILURE) {
             takeScreenshot(result.getName());
         }
 
-        if(getDriver()!=null){
+        if (getDriver() != null) {
             getDriver().quit();
+            driver.remove();
         }
     }
 
-    public void takeScreenshot(String testName){
-        try{
-
-            TakesScreenshot ts = (TakesScreenshot)getDriver();
+    public void takeScreenshot(String testName) {
+        try {
+            TakesScreenshot ts = (TakesScreenshot) getDriver();
             File src = ts.getScreenshotAs(OutputType.FILE);
 
             String time = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-
-            File dest = new File("target/screenshots/"+testName+"_"+time+".png");
+            File dest = new File("target/screenshots/" + testName + "_" + time + ".png");
 
             dest.getParentFile().mkdirs();
-
             Files.copy(src.toPath(), dest.toPath());
 
-        }catch(Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
